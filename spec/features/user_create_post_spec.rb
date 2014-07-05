5ae5277fcffc1935945e339dfb68f277a1a85624
André Vieira <andvieira.16@gmail.com>
2014-07-05 10:17:50 +0100

    resolved tests

--- spec/features/user_create_post_spec.rb
+++ spec/features/user_create_post_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 feature 'create post:'  do
   scenario 'valid' do
+    user = User.create email: 'email@email.com', password: 'passTeste'
+
     visit new_post_path
+
+    within 'form' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_on 'Sign in'
+    end
+
     fill_in 'Title', with: 'title of post'
     fill_in 'Body', with: 'body of post'
     click_on 'Create Post'

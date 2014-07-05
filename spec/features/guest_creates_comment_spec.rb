34b0d2b523ee2aad398980d376e31603eba3ab09
Tiago Duque <tiago.fcduque@gmail.com>
2014-07-05 12:06:02 +0100

    resolved test of comments

--- spec/features/guest_creates_comment_spec.rb
+++ spec/features/guest_creates_comment_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 feature 'Guest creates comment' do
   scenario 'in a post\'s page' do
+  	user = User.create email: 'email@email.com', password: 'passTeste'
     post = Post.create title: 'cenas'
     visit post_path(post)
 
-    fill_in 'comment_body', with: 'comment'
-    click_on 'Add comment'
+	within 'form' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_on 'Sign in'
+    end
+
+	within 'form' do
+    	fill_in 'comment_body', with: 'comment'
+    	click_on 'Add comment'
+	end
 
     expect(page).to have_text 'comment'
   end
